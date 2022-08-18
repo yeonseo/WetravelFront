033d66d433780f80ee9ddab74cddf95cad7b6ac7
yeonseo <yulga_nara@naver.com>
2022-08-18 14:34:34 +0900

    Header 수정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="main-top logo">
+    <div class="main-top logo" v-if="isBack">
+        <i class="fa-solid fa-arrow-left mr-10" @click="$router.go(-1)"></i>
+        <span>{{title}}</span>
+    </div>
+
+    <div class="main-top logo" v-else>
         <span>We Travel</span>
     </div>
 </template>
+<script>
+export default {
+    props: ['isBack', 'title'],
+}
+</script>
